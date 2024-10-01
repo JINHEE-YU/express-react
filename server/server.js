fc9d2265275e7dc49f47e2c054b19e2e8344395a
JINHEE-YU <yujh2292@gmail.com>
2024-10-01 16:15:22 +0900

    🎉 express,react 프로젝트 구성

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const apiRouter = require('./Router/api');
+
+app.use('/api', apiRouter);
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
