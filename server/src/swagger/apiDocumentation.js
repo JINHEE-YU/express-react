bcd4a2727d440089bc680c0937ebf9778a12a312
JINHEE-YU <yujh2292@gmail.com>
2024-10-07 16:42:28 +0900

    🔧 테스트용 swagger 주석 추가 - /api/test/db-test - /api/test/log

--- server/src/swagger/apiDocumentation.js
+++ server/src/swagger/apiDocumentation.js
@@ -58,3 +58,41 @@
  *                   type: string
  *                   example: "hello api router"
  */
+/**
+ * @swagger
+ * /api/test/db-test:
+ *   get:
+ *     tags: [API]
+ *     summary: DB 정보 확인
+ *     description: 연결된 DB 버전을 반환합니다.
+ *     responses:
+ *       200:
+ *         description: 성공적으로 응답
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 version:
+ *                   type: string
+ *                   example: "PostgreSQL 17.0 on x86_64-windows, compiled by msvc-19.41.34120, 64-bit"
+ */
+/**
+ * @swagger
+ * /api/test/log:
+ *   get:
+ *     tags: [API]
+ *     summary: log 확인
+ *     description: 프로젝트에 설정된 로그에 맞게 파일에 log를 쌓습니다.<br>(로그파일 확인해보기)
+ *     responses:
+ *       200:
+ *         description: 성공적으로 응답
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 version:
+ *                   type: string
+ *                   example: "hello log"
+ */
